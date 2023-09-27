2b0535457a9eced67ce01b44c928752a25e0cebd
mauldoto <maulanaly97@gmail.com>
2023-09-27 11:42:43 +0700

    modify config and git ignore

---
+++ app/config/config.example.php
@@ -0,0 +1,15 @@
+<?php
+
+define('BASEURL', '');
+
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_SERVICE', '');
+
+define('TYPE_INPUT', '');
+
+define('NAMA_PT', ''); // diisi nama PT misalkan: PT GCM, SMG, SLM, SBE, SJE
+
+
+// define('DB_NAME', 'test');
