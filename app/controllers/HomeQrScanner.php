40d851cfae694c466170c77ea9b8377c3741a93c
Mauldoto <maulanaly97@gmail.com>
2023-09-28 22:11:13 +0700

    add model and controller

--- app/controllers/HomeQrScanner.php
+++ app/controllers/HomeQrScanner.php
@@ -11,48 +11,35 @@ class HomeQrScanner extends Controller
 
     public function input()
     {
-        $data = json_decode($_POST['submited_data'], true);
-        if (count($data) < 4) {
-            Flasher::setMessage('failed', 'Semua kategori belum diberi penilaian', 'danger');
-            header('location: ' . BASEURL . '/');
-            exit;
-        }
+        // $data = json_decode($_POST['submited_data'], true);
+        // if (count($data) < 4) {
+        //     Flasher::setMessage('failed', 'Semua kategori belum diberi penilaian', 'danger');
+        //     header('location: ' . BASEURL . '/');
+        //     exit;
+        // }
 
-        if ($this->model('SurveyTamuModel')->saveData($data) <= 0) {
-            Flasher::setMessage('Failed,', 'Check your input', 'danger');
-            header('location: ' . BASEURL . '/tamu');
-            exit;
-        }
+        // if ($this->model('SurveyTamuModel')->saveData($data) <= 0) {
+        //     Flasher::setMessage('Failed,', 'Check your input', 'danger');
+        //     header('location: ' . BASEURL . '/tamu');
+        //     exit;
+        // }
 
-        Flasher::setMessage('Successfully', 'Created', 'success');
-        header('location: ' . BASEURL . '/tamu');
-        exit;
+        // Flasher::setMessage('Successfully', 'Created', 'success');
+        // header('location: ' . BASEURL . '/tamu');
+        // exit;
     }
 
     public function report()
     {
-        $master = [];
+        // var_dump($_SERVER['REQUEST_URI']);
+        $dataFull = explode('?', $_SERVER['REQUEST_URI']);
+        $shorterData = explode('&', $dataFull[1]);
 
-        $date = explode('=', $_SERVER['REQUEST_URI']);
-
-        $data = $this->model('SurveyTamuModel')->getDataByDateRange($date);
-        foreach ($data as $key => $kepuasan) {
-            foreach ($kepuasan as $key2 => $detail) {
-                $kategori = explode("_", $key2);
-
-                if (isset($master[$kategori[0]])) {
-                    if (isset($master[$kategori[0]][$kategori[1]])) {
-                        $master[$kategori[0]][$kategori[1]] += intval($detail);
-                    } else {
-                        $master[$kategori[0]][$kategori[1]] = intval($detail);
-                    }
-                } else {
-                    $master[$kategori[0]] = [];
-                    $master[$kategori[0]]["name"] = $kategori[0];
-                }
-            }
+        $results = $this->model('HomeQrScanner')->getPenghuniRumah($shorterData);
+        foreach ($results as $key => $detail) {
+            $detail['details'] = $this->model('HomeQrScanner')->getDetailPenghuni([$detail['KODERUMAH'], $detail['NOPINTU']]);
         }
 
-        echo json_encode(["data" => $master]);
+        // echo json_encode(["data" => $data]);
     }
 }
